33561feae62ed1a31427675ad8943d49272a7edb
ranchana_k <jb.ranchana@gmail.com>
2025-03-30 13:29:47 +0700

    Edit variables

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,12 +6,12 @@ python3 -m pip install -r requirements.txt
 echo "🚀 Launching Dataflow job..."
 
 python3 main.py \
-  --aggregation_window_sec=${_AGG_WINDOW:-60} \
-  --alert_window_sec=${_ALERT_WINDOW:-1} \
+  --aggregation_window_sec=${AGG_WINDOW:-60} \
+  --alert_window_sec=${ALERT_WINDOW:-1} \
   --runner=DataflowRunner \
   --project=$PROJECT_ID \
-  --region=${_REGION} \
-  --temp_location=gs://${_BUCKET}/temp \
-  --staging_location=gs://${_BUCKET}/staging \
+  --region=${REGION} \
+  --temp_location=gs://${BUCKET}/temp \
+  --staging_location=gs://${BUCKET}/staging \
   --job_name=streaming-purchase-alerts-$(date +%Y%m%d%H%M%S)
 echo "🚀 Done Submitting Dataflow job..."
