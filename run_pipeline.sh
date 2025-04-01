16daa193bea48f3327715b7e19ed14990cc396f8
Ranchana Kiriyapong <68572758+ranchana-k@users.noreply.github.com>
2025-04-01 15:57:08 +0700

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,6 +6,9 @@ python3 -m pip install -r requirements.txt
 echo "🚀 Launching Dataflow job..."
 
 python3 main.py \
+  --input_topic=projects/$PROJECT_ID/topics/streaming-events \
+  --output_table=$PROJECT_ID:dataflow_test.purchase_summary \
+  --alert_topic=projects/$PROJECT_ID/topics/high-volumn-alerts \
   --aggregation_window_sec=${AGG_WINDOW:-60} \
   --alert_window_sec=${ALERT_WINDOW:-1} \
   --runner=DataflowRunner \
